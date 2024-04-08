adac3afc6591b3eb6c4a05ed98d3979563e52323
NUNO SOUSA <nunomsousa@ua.pt>
2024-04-08 03:21:59 +0100

    docker

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# market-analysis
+# market-analysis for Fuel Link
+
+To start the service run
+
+```bash
+docker compose up -d
+```
+
+Currently access to influxdb is defined through the enviroment variables in docker-compose, if those change in the influxdb service they need to change here too.
+
+Predictions are calculated by calling /predict with a bucket, measurement and field like this:
+
+```bash
+localhost:5000/predict?bucket=bucketName&measurement=measurementName&field=fieldName
+```
+
+this will query the db and calculate prediction values for the next 15 days, those are returned like this:
+
+```bash
+{
+    "predictions": [
+        {
+            "ds": "Wed, 03 Apr 2024 00:00:00 GMT",
+            "yhat": 1.7213467119261394
+        }
+    ]
+}
+```
+Currently is the bucket that is select does not currently exist, it will create it and load it with data from a .csv file, also there is an example on how to use this in the home directory of this service
--- app/app.py
+++ app/app.py
@@ -3,20 +3,16 @@ from datetime import datetime
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from prophet import Prophet
-from prophet.serialize import model_to_json, model_from_json
-from datetime import datetime, timedelta, timezone
-# import pandas as pd
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
-read_token = "hm-Il0Rq9lXQhmKR4-gnXTgjHiaJ92Qv8fZ2b3ga1DBP5gqf7f3V6DHsOYvpW_afnaCEmwbMefMCOZQV49y8Gw=="
-write_token = "hm-Il0Rq9lXQhmKR4-gnXTgjHiaJ92Qv8fZ2b3ga1DBP5gqf7f3V6DHsOYvpW_afnaCEmwbMefMCOZQV49y8Gw=="
-org = "FuelLink"
-url = "http://localhost:8086"
+INFLUXDB_URL = os.getenv('INFLUXDB_URL')
+INFLUXDB_TOKEN = os.getenv('INFLUXDB_TOKEN')
+INFLUXDB_ORG = os.getenv('INFLUXDB_ORG')
 
-read_client = InfluxDBClient(url=url, token=read_token, org=org)
-write_client = InfluxDBClient(url=url, token=write_token, org=org)
-write_api = write_client.write_api(write_options=SYNCHRONOUS)
+# print(f"URL: {INFLUXDB_URL}, Token: {INFLUXDB_TOKEN}, Org: {INFLUXDB_ORG}")
 
 
 @app.route('/')
@@ -25,15 +21,36 @@ def home():
 	return render_template('index.html')
 
 
-@app.route('/predict')
+@app.route('/predict', methods=["GET"])
 def predict():
+	
+	read_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG, timeout=60000)
 
 	bucket = request.args.get('bucket')
 	measurement = request.args.get('measurement')
 	field = request.args.get('field')
-
+	
 	if not measurement or not field:
 		return jsonify({'error': 'Measurement and field parameters are required'}), 400
+
+	# If bucket does not exist, imports the Postos.csv into the database to run the model
+	buckets_api = read_client.buckets_api()
+	if not buckets_api.find_bucket_by_name(bucket):
+		buckets_api.create_bucket(bucket_name=bucket, org=INFLUXDB_ORG)
+		write_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG, timeout=60000)
+		write_api = write_client.write_api(write_options=SYNCHRONOUS)
+
+		df = pd.read_csv('Postos.csv')
+
+		points = []
+		for index, row in df.iterrows():
+			point = Point(measurement)\
+				.field(field, float(row['y']))\
+				.time(row['ds'], WritePrecision.NS)
+			points.append(point)
+
+		write_api.write(bucket=bucket, org=INFLUXDB_ORG, record=points)
+		write_client.close()
 
 	# Construct the Flux query
 	query = f'''
@@ -44,7 +61,7 @@ def predict():
 	'''
 
 	# Query InfluxDB
-	result = read_client.query_api().query_data_frame(query=query, org=org)
+	result = read_client.query_api().query_data_frame(query=query, org=INFLUXDB_ORG)
 	if result.empty:
 		return jsonify({'error': 'No data found for the specified date range'}), 404
 
@@ -65,40 +82,15 @@ def predict():
 	predictions = forecast[['ds', 'yhat']].tail(15)
 	predictions_list = predictions.to_dict('records')
 
-	for prediction in predictions_list:
-		print(prediction)
-		# Parse the date and value
-		date = prediction['ds']
-		yhat = prediction['yhat']
-
-		# Create a Point
-		point = Point("Predictions")\
-			.field("yhat", yhat)\
-			.time(date, WritePrecision.NS)
-
-		write_api.write(bucket='Predictions', org=org, record=point)
-
-	query = '''
-			from(bucket: "Predictions")
-			|> range(start: now(), stop: 15d)
-			|> filter(fn: (r) => r["_measurement"] == "Predictions")
-			|> filter(fn: (r) => r["_field"] == "yhat")
-			'''
-	
-	result = read_client.query_api().query(org=org, query=query)
-
-	# Process the results
-	for table in result:
-		for record in table.records:
-			print(f'Time: {record.get_time()}, Predicted Value: {record.get_value()}')
-
-
-
+	read_client.close()
 	return jsonify({'predictions': predictions_list})
 
 
 @app.route('/data')
 def data():
+
+	write_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+	write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
 	date = request.args.get("date")
 	price = request.args.get("price")
@@ -112,7 +104,8 @@ def data():
 		.field("y", float(price))\
 		.time(date_obj, WritePrecision.NS)
 
-	write_api.write(bucket="Gas-Prices", org=org, record=point)
+	write_api.write(bucket="Gas-Prices", org=INFLUXDB_ORG, record=point)
+	write_client.close()
 	
 	return jsonify({"message": "Data submitted successfully"}), 200
 
