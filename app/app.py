09251d789ad02fe11a0a7e9f98669e4428e65ff2
LAPTOP-M8283F5H\nunom <nunomsousa@ua.pt>
2024-04-11 09:22:09 +0100

    Users list and dynamic access

--- app/app.py
+++ app/app.py
@@ -8,12 +8,7 @@ import os
 
 app = Flask(__name__)
 
-INFLUXDB_URL = os.getenv('INFLUXDB_URL')
-INFLUXDB_TOKEN = os.getenv('INFLUXDB_TOKEN')
-INFLUXDB_ORG = os.getenv('INFLUXDB_ORG')
-
-# print(f"URL: {INFLUXDB_URL}, Token: {INFLUXDB_TOKEN}, Org: {INFLUXDB_ORG}")
-
+Users = {}	
 
 @app.route('/')
 def home():
@@ -24,20 +19,32 @@ def home():
 @app.route('/predict', methods=["GET"])
 def predict():
 	
-	read_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG, timeout=60000)
+	org = request.args.get('org')
+	token = request.args.get('token')
+	days = request.args.get('days',default=15)
 
-	bucket = request.args.get('bucket')
-	measurement = request.args.get('measurement')
-	field = request.args.get('field')
+	if not org or not token:
+		return jsonify({'error': 'org and token parameters are required'}), 400
 	
-	if not measurement or not field:
-		return jsonify({'error': 'Measurement and field parameters are required'}), 400
+	print(Users.get(org))
+	
+	if not Users.get(org):
+		return jsonify({'error': 'organization not found'}), 404
+	
+	url= Users[org]["url"]
+	bucket = Users[org]["bucket"]
+	measurement = Users[org]["measurement"]
+	field = Users[org]["field"]
+
+	read_client = InfluxDBClient(url=url, token=token, org=org, timeout=60000)
+	
+	
 
 	# If bucket does not exist, imports the Postos.csv into the database to run the model
 	buckets_api = read_client.buckets_api()
 	if not buckets_api.find_bucket_by_name(bucket):
-		buckets_api.create_bucket(bucket_name=bucket, org=INFLUXDB_ORG)
-		write_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG, timeout=60000)
+		buckets_api.create_bucket(bucket_name=bucket, org=org)
+		write_client = InfluxDBClient(url=url, token=token, org=org, timeout=60000)
 		write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
 		df = pd.read_csv('Postos.csv')
@@ -49,7 +56,7 @@ def predict():
 				.time(row['ds'], WritePrecision.NS)
 			points.append(point)
 
-		write_api.write(bucket=bucket, org=INFLUXDB_ORG, record=points)
+		write_api.write(bucket=bucket, org=org, record=points)
 		write_client.close()
 
 	# Construct the Flux query
@@ -61,7 +68,7 @@ def predict():
 	'''
 
 	# Query InfluxDB
-	result = read_client.query_api().query_data_frame(query=query, org=INFLUXDB_ORG)
+	result = read_client.query_api().query_data_frame(query=query, org=org)
 	if result.empty:
 		return jsonify({'error': 'No data found for the specified date range'}), 404
 
@@ -73,41 +80,85 @@ def predict():
 	m = Prophet()
 	m.fit(df)
 
-	future = m.make_future_dataframe(periods=15)
+	future = m.make_future_dataframe(periods=int(days))
 	future.tail()
 
 	forecast = m.predict(future)
 
 
-	predictions = forecast[['ds', 'yhat']].tail(15)
+	predictions = forecast[['ds', 'yhat']].tail(int(days))
 	predictions_list = predictions.to_dict('records')
 
 	read_client.close()
 	return jsonify({'predictions': predictions_list})
 
 
-@app.route('/data')
-def data():
+@app.route('/addClient', methods=['GET'])
+def addClient():
+	org = request.args.get("org")
+	url = request.args.get("url")
+	bucket = request.args.get("bucket")
+	measurement = request.args.get("measurement")
+	field = request.args.get("field")
 
-	write_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
-	write_api = write_client.write_api(write_options=SYNCHRONOUS)
+	if not url or not org or not bucket or not measurement or not field:
+		return jsonify({"error": "url, org, bucket, measurement and field parameters are required."}), 400
 
-	date = request.args.get("date")
-	price = request.args.get("price")
+	newUser = {"url": url, "bucket": bucket, "measurement": measurement, "field": field}
+	Users[org] = newUser
 
-	if not date or not price:
-		return jsonify({"error": "Date and price parameters are required."}), 400
+	return jsonify({"message": "User added successfully"}), 200
 
-	# date_str = date  # Example date string
-	date_obj = datetime.strptime(date, '%Y-%m-%d')
-	point = Point("Prices")\
-		.field("y", float(price))\
-		.time(date_obj, WritePrecision.NS)
 
-	write_api.write(bucket="Gas-Prices", org=INFLUXDB_ORG, record=point)
-	write_client.close()
+@app.route('/updateClient', methods=['GET'])
+def updateClient():
+	org = request.args.get("org")
+	url = request.args.get("url")
+	bucket = request.args.get("bucket")
+	measurement = request.args.get("measurement")
+	field = request.args.get("field")
+
+	if not org:
+		return jsonify({"error": "org parameter is required."}), 400
 	
-	return jsonify({"message": "Data submitted successfully"}), 200
+	if not Users.get(org):
+		return jsonify({'error': 'organization not found'}), 404
+	
+	if url:
+		Users[org]['url'] = url
+	if bucket:
+		Users[org]['bucket'] = bucket
+	if measurement:
+		Users[org]['measurement'] = measurement
+	if field:
+		Users[org]['field'] = field
+
+	return jsonify({"message": "User updated successfully"}), 200
+
+	
+
+# @app.route('/data')
+# def data():
+
+# 	write_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+# 	write_api = write_client.write_api(write_options=SYNCHRONOUS)
+
+# 	date = request.args.get("date")
+# 	price = request.args.get("price")
+
+# 	if not date or not price:
+# 		return jsonify({"error": "Date and price parameters are required."}), 400
+
+# 	# date_str = date  # Example date string
+# 	date_obj = datetime.strptime(date, '%Y-%m-%d')
+# 	point = Point("Prices")\
+# 		.field("y", float(price))\
+# 		.time(date_obj, WritePrecision.NS)
+
+# 	write_api.write(bucket="Gas-Prices", org=INFLUXDB_ORG, record=point)
+# 	write_client.close()
+	
+# 	return jsonify({"message": "Data submitted successfully"}), 200
 
 if __name__ == '__main__':
 	app.run(debug=True)
